6499d9fd6a1f70fe6d19b610b78c7d657e77bc54
Fernando Veras <nandogomescode@gmail.com>
2025-07-28 23:54:58 -0300

    Criados os primeiros services da classe Gastos

---
+++ Gastos/Gastos/GastoMensal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gastos.GastosMensais
+{
+    public class GastoMensal
+    {
+
+        public GastoMensal() { }
+
+        public void GastoMensalService()
+        {
+
+        }
+
+    }
+}
--- fc-logic/fc-logic/fc-logic/Gastos.cs
+++ fc-logic/fc-logic/fc-logic/Gastos.cs
@@ -1,13 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Gastos.GastoParcela;
+using Gastos.GastosMensais;
+using Gastos.GastoUnico;
+using Gastos.Salario;
 
 namespace fc_logic.Gastos
 {
     public class Gastos
     {
+
+        #region PRIVATE
+        private decimal _gastosTotaisMensal { get; set; }
+        private int _option { get; set; }
+
+        private GastoMensal _mensal = new GastoMensal();
+        private GastoParcela _parcela = new GastoParcela();
+        private GastoUnico _gastoUnico = new GastoUnico();
+        private Salario _salario = new Salario();
+        #endregion
 
         #region CONSTRUCTOR
         public Gastos()
@@ -21,7 +30,45 @@ namespace fc_logic.Gastos
         public void GastosMain()
         {
 
+            do
+            {
 
+                Console.Clear();
+                Console.WriteLine("Bem vinda a página de gastos");
+                Console.WriteLine("1) Adicionar gasto mensal\n2) Adicionar gasto único (feito apenas uma vez nesse mês)\n3) Adicionar gasto em parcelas\n4) Alterar salário");
+                Console.WriteLine($"\n\nGanhos totais mensal: {this._gastosTotaisMensal}");
+
+                Console.WriteLine("Escolha sua opção: ");
+                _option = Convert.ToInt32(Console.ReadLine());
+
+                this.GastosOptions(this._option);
+
+            } while(_option != 0);
+
+        }
+
+        private void GastosOptions(int option)
+        {
+
+
+
+            switch(option)
+            {
+
+                case 1:
+                    this._mensal.GastoMensalService();
+                    break;
+                case 2:
+                    this._gastoUnico.GastoUnicoService();
+                    break;
+                case 3:
+                    this._parcela.ParcelaService();
+                    break;
+                case 4:
+                    this._salario.SalarioService();
+                    break;
+
+            }
 
         }
 
